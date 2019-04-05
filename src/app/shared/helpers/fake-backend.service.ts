c5be3457ccabceaebf419c78116b8ec06da9dbcb
Holub Jiří <holub.jiri@vumop.local>
2019-04-05 14:29:02 +0200

    edit service

--- src/app/shared/helpers/fake-backend.service.ts
+++ src/app/shared/helpers/fake-backend.service.ts
@@ -20,14 +20,11 @@ export class FakeBackendService implements HttpInterceptor {
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-
-
     /**
-     * 
+     *
      * http://jasonwatmore.com/post/2018/05/16/angular-6-user-registration-and-login-example-tutorial#alert-service-ts
-     * 
+     *
      */
-
 
     console.log("Fake backend - request.url", request.url);
 
