ae252c4dee00e3c66109fdeefed91c7be4f468af
Holub Jiří <holub.jiri@vumop.local>
2019-05-02 17:31:03 +0200

    Modern-app: setting and change url foto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://test65.vumop.cz/monitoring/public"
+  apiUrl: "test65.vumop.cz/monitoring/public"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost/monitoring/public"
+  apiUrl: "localhost/monitoring/public"
 };
 
 /*
